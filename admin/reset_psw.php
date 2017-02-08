160df93bb42d8032819bcf9cc7461c35d7e22be7
Galtavoz <galtarossa94@gmail.com>
2017-02-08 13:44:08 +0100

    Update reset_psw.php

--- admin/reset_psw.php
+++ admin/reset_psw.php
@@ -1,7 +1,17 @@
 <?php
-$connect=mysql_connect('localhost','root','') or die ('Errore connessione');
-mysql_set_charset('utf8', $connect);
-$db=mysql_select_db('progettotecweb',$connect) or die('Errore BD');
+
+$connessione = mysqli_connect("localhost","mbodian","iy9Paeka6ooR7Wi0", "mbodian");
+mysqli_set_charset('utf8', $connessione);
+if (!$connessione) die("<h1>Connessione a Mysql non riuscita</h1> ".mysqli_connect_error());
+else {
+$db = mysqli_select_db($connessione, "mbodian");
+if (!$db) die("<h1>Connessione al Database non riuscita</h1> ".mysqli_connect_error());
+}
+	
+
+
+
+$db=mysqli_select_db('progettotecweb',$connessione) or die('Errore BD');
 session_start();
 if (empty($_POST['old_password'])) {
 	exit("Attenzione inserire vecchia password");
@@ -13,21 +23,21 @@ if (empty($_POST['check_password'])) {
 	exit("Attenzione inserire nuovamente la password");
 }
 $password = md5($_POST['old_password']);
-$user = mysql_real_escape_string($_SESSION['username']);
+$user = mysqli_real_escape_string($_SESSION['username']);
 
 $old_password_db = "SELECT username,password FROM admin WHERE username = '$user' AND password = '$password'";
-$old_password_db_query = mysql_query($old_password_db) or die(mysql_error());
-if (!mysql_num_rows($old_password_db_query)) {
+$old_password_db_query = mysqli_query($old_password_db) or die(mysqli_error());
+if (!mysqli_num_rows($old_password_db_query)) {
 	exit("Attenzione la vecchia password inserita non e' corretta");
 }
 if ($_POST['new_password'] != $_POST['check_password']) {
 	exit("Le password non correispondono, prego reinserirle");
 }
 $new_password = md5($_POST['new_password']);
-$query_update = mysql_query("UPDATE admin SET password = '$new_password' WHERE username = '".$user."'")or die();
+$query_update = mysqli_query("UPDATE admin SET password = '$new_password' WHERE username = '".$user."'")or die();
 if(!$query_update)
-	echo mysql_error();
-if (!mysql_affected_rows()) {
+	echo mysqli_error();
+if (!mysqli_affected_rows()) {
 	exit("Errore nell'aggionamento di".$_SESSION['username']."");
 } else {
 	session_destroy();
