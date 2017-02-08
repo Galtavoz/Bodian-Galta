ae8355c9bb3e408615d73222ae920f86b699e3f6
Galtavoz <galtarossa94@gmail.com>
2017-02-08 15:37:48 +0100

    Add files via upload

--- admin/config.php
+++ admin/config.php
@@ -1,10 +1,9 @@
 <?php
-
-$connessione = mysql_connect("localhost","root","" );
-
-if (!$connessione) die("<h1>Connessione a Mysql non riuscita</h1> ".mysql_error());
+$connessione = mysqli_connect("localhost","mbodian","iy9Paeka6ooR7Wi0", "mbodian");
+if (!$connessione) die("<h1>Connessione a Mysql non riuscita</h1> ".mysqli_connect_error());
 else {
-$db = mysql_select_db("progettotecweb",$connessione);
-if (!$db) die("<h1>Connessione al Database non riuscita</h1> ".mysql_error());
-}	
+$db = mysqli_select_db($connessione, "mbodian");
+if (!$db) die("<h1>Connessione al Database non riuscita</h1> ".mysqli_connect_error());
+}
+	
 ?>
